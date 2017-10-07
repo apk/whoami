c08a1c71b809f5b6c9fa31c150707b1536b92b5a
Andreas Krey <a.krey@gmx.de>
2017-10-07 22:08:02 +0200

    show some request headers and details as well

--- http.go
+++ http.go
@@ -17,9 +17,20 @@ func main() {
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
-    })
+        fmt.Fprintf(w, "I'm %s\n", hostname)
 
+        fmt.Fprintf(w, "\n")
+        fmt.Fprintf(w, "Method: %s\n", r.Method)
+        fmt.Fprintf(w, "Proto: %s\n", r.Proto)
+        fmt.Fprintf(w, "Host: %s\n", r.Host)
+        fmt.Fprintf(w, "RemoteAddr: %s\n", r.RemoteAddr)
+        fmt.Fprintf(w, "\n")
+        for k, v := range r.Header {
+            for _, a := range v {
+                 fmt.Fprintf(w, "%s: %s\n", k, a)
+            }
+        }
+    })
 
     log.Fatal(http.ListenAndServe(":" + port, nil))
 }
